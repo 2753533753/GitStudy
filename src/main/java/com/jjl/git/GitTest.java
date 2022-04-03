9284ec289c78e27fdccb41a47946a8f7b6784c17
jinjialong <2237427554@qq.com>
2022-04-03 11:48:38 +0800

    first commit

---
+++ src/main/java/com/jjl/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jjl.git;
+
+/**
+ * @author jinjialong
+ * @create 2022-04-03 11:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
