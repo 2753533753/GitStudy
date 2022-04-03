23eefbbb22f0852bb254a04dfd3593d307c88528
jinjialong <2237427554@qq.com>
2022-04-03 11:51:35 +0800

    second commit

--- src/main/java/com/jjl/git/GitTest.java
+++ src/main/java/com/jjl/git/GitTest.java
@@ -7,5 +7,6 @@ package com.jjl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
